42e5faf751f0855e4b9e65ba560c71894789c0c2
Matthew Arvidson <Matthew.Arvidson@gmail.com>
2017-08-29 17:45:42 -0600

    Completes Phase One

--- java.js
+++ java.js
@@ -4,48 +4,84 @@
 // Enter button
 var enterButton = document.querySelector('.enter-button');
 // Main Content Container
+var mainContent = document.querySelector('.linked-list-tile-container');
 // Website title
+var websiteTitleUserInput = document.querySelector('.website-title-input');
 // Website URL
+var websiteUrlUserInput = document.querySelector('.website-url-input');
 // Clear Button
 
 
 //****Objects****
 
 
-// Function that builds Idea Boxes
+
 
 
 //****Event Listeners****
 
 
-// Clear Button Event Listener
-
+// Delete Button Event Listener
+// deleteButton.addEventListener('click', removeCard);
 // Enter Button Event Listner
 enterButton.addEventListener('click', generateCard);
 // Website URL Event Listener
-
+// websiteTitleUserInput.addEventListener('keypress' enableEnterButton());
 // Website Title Event Listener
+// websiteUrlUserInput.addEventListener('keypress' enableEnterButton());
+
+
 
 // Main Content Container Event Listener
 
 
 //****Functions****
 
+// Function that builds Linked List Boxes
 function generateCard(event) {
   event.preventDefault();
   var websiteTitle = document.querySelector('.website-title-input').value;
   var websiteUrl = document.querySelector('.website-url-input').value;
-  var mainContent = document.querySelector('.linked-list-tile-container');
   mainContent.insertAdjacentHTML('afterbegin', `<section class="linked-list-box">
-    <article class="website-title-output"><h2>${websiteTitle}</h2></article>
-    <article class="website-url-output">${websiteUrl}</article>
-    <span class="read-delete-links"><article class="read-link"><h3 class="read"><a>Read</a></h3></article>
-    <article class="delete-link"><h3 class="delete"><a>Delete</a></h3></article></span>
-  </section>`);
+      <h2 class="website-title-output">${websiteTitle}</h2>
+      <a class="website-url-output" href="${websiteUrl}">${websiteUrl}</a>
+      <span class="read-delete-links">
+      <a class="read read-link">Read</a>
+      <a class="delete delete-link">Delete</a>
+      </span>
+    </section>`);
+  clearInputsResetFocus();
 };
 
 
+
+
+//Clear Input Fields And Reset Focus
+function clearInputsResetFocus() {
+  $('.website-title-input').val('');
+  $('.website-url-input').val('');
+  $('.website-title-input').focus();
+}
+
+
 // Clear Read Links
+
+//Delete A Link Boxe
+
+$(".linked-list-tile-container").on('click', '.delete', deleteLink);
+
+$(".linked-list-tile-container").on('click', '.read', toggleMarkAsRead);
+
+function deleteLink () {
+  $(this).parent().parent().parent().remove();
+}
+
+//Toggle Mark As Read
+function toggleMarkAsRead() {
+  $(this).closest('section').toggleClass('linked-list-box-read');
+}
+
+//On click of "read" toggle text color to red, and the container background-color to grey
 
 // Total Number Of Link Boxes
 
