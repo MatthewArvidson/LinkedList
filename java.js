40eb162fb64e2ee2119fe0d18932d0ab1ebd1366
Matthew Arvidson <Matthew.Arvidson@gmail.com>
2017-08-30 15:58:50 -0600

    page is now responsive

--- java.js
+++ java.js
@@ -1,39 +1,15 @@
 //****Global Variables****
 
-
-// Enter button
 var enterButton = document.querySelector('.enter-button');
-// Main Content Container
 var mainContent = document.querySelector('.linked-list-tile-container');
-// Website title
 var websiteTitleUserInput = document.querySelector('.website-title-input');
-// Website URL
 var websiteUrlUserInput = document.querySelector('.website-url-input');
-// Clear Button
-
 
 //****Objects****
 
-
-
-
-
 //****Event Listeners****
 
-
-// Delete Button Event Listener
-// deleteButton.addEventListener('click', removeCard);
-// Enter Button Event Listner
 enterButton.addEventListener('click', userInputErrorMessage);
-// Website URL Event Listener
-// websiteTitleUserInput.addEventListener('keypress' enableEnterButton());
-// Website Title Event Listener
-// websiteUrlUserInput.addEventListener('keypress' enableEnterButton());
-
-
-
-// Main Content Container Event Listener
-
 
 //****Functions****
 
@@ -49,7 +25,6 @@ function userInputErrorMessage() {
   }
 };
 
-// Function that builds Linked List Boxes
 function generateCard() {
   var websiteTitle = document.querySelector('.website-title-input').value;
   var websiteUrl = document.querySelector('.website-url-input').value;
@@ -64,20 +39,11 @@ function generateCard() {
   clearInputsResetFocus();
 };
 
-
-
-
-//Clear Input Fields And Reset Focus
 function clearInputsResetFocus() {
   $('.website-title-input').val('');
   $('.website-url-input').val('');
   $('.website-title-input').focus();
 }
-
-
-// Clear Read Links
-
-//Delete A Link Boxe
 
 $(".linked-list-tile-container").on('click', '.delete', deleteLink);
 
@@ -87,21 +53,6 @@ function deleteLink () {
   $(this).parent().parent().remove();
 }
 
-//Toggle Mark As Read
 function toggleMarkAsRead() {
   $(this).closest('section').toggleClass('linked-list-box-read');
 }
-
-//On click of "read" toggle text color to red, and the container background-color to grey
-
-// Total Number Of Link Boxes
-
-// Validate URL
-
-// Correct Format Of The Web Address
-
-// Control The Error State
-
-// Validate Form
-
-// Website Box Links
